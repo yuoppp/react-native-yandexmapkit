872127facb766848c301fb088da53884905d1c14
Yuri Tinyukov <y.tinyukov@qsoft.ru>
2018-04-02 17:20:59 +0300

    RN 0.47

--- android/src/main/java/camp/kuznetsov/rn/yandexmapkit/RNYandexMapKitPackage.java
+++ android/src/main/java/camp/kuznetsov/rn/yandexmapkit/RNYandexMapKitPackage.java
@@ -13,17 +13,14 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNYandexMapKitPackage implements ReactPackage {
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(new RNYandexMapKitModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         ArrayList<ViewManager> viewManagers = new ArrayList<>();
         viewManagers.add(new RNYandexMapKitManager());
